501bcecf28f796d2723ca635077d6645bb9e138f
TGRCDev <tgrc@tgrc.dev>
2022-01-16 19:19:54 -0800

    Change return type for get_steam_users_info
    Documentation

--- src/api/steam.rs
+++ src/api/steam.rs
@@ -10,6 +10,7 @@ use rocket::{
 use std::{
     ops::Deref,
     time::Duration,
+    collections::HashMap,
 };
 use reqwest::{ ClientBuilder, Client, StatusCode };
 use itertools::Itertools;
@@ -17,18 +18,30 @@ use figment::Figment;
 
 use crate::structs::SteamUser;
 
-// TODO: Documentation
+pub type SteamID = u64;
 
+/// Various errors that can occur with the SteamClient
 #[derive(Debug, Serialize)]
 #[serde(tag = "type")]
 pub enum SteamError {
+    /// The steam user with the given Steam id has an inaccessible games list
     NonPublicUser {
-        id: u64
+        id: SteamID,
     },
+
+    /// This WhatCanWePlay instance doesn't have a Steam webkey defined
     MissingWebKey,
+
+    /// This WhatCanWePlay instance has an invalid Steam webkey defined
     BadWebKey,
+
+    /// An error occurred while constructing the Steam request client
     ClientBuildError(String),
+
+    /// Steam returned an unparseable response
     BadSteamResponse(String),
+
+    /// Steam had an unhandled error code response
     SteamErrorStatus{
         code: u16,
         message: String,
@@ -61,6 +74,7 @@ impl From<reqwest::Error> for SteamError
     }
 }
 
+/// Deref wrapper for a reqwest Client that provides shortcut functions to Steam API endpoints
 pub struct SteamClient(Client, String);
 impl Deref for SteamClient {
     type Target = Client;
@@ -71,15 +85,24 @@ impl Deref for SteamClient {
     }
 }
 
+/// Result type for returning to the HTTP client
 pub type APIResult<T> = std::result::Result<Json<T>, Custom<Json<SteamError>>>;
+/// Result type for processing results from Steam API
 pub type SteamResult<T> = std::result::Result<T, SteamError>;
 
+
 impl SteamClient {
+    /// Attempt to construct a new SteamClient from the given Figment config
+    /// 
+    /// Will return Err if the config doesn't contain a key named 'steam_webkey',
+    /// or if the webkey fails to authenticate with Steam API
     pub async fn new(figment: &Figment) -> SteamResult<SteamClient> {
         let webkey: String = figment
             .extract_inner("steam_webkey")
             .map_err(|_err| SteamError::MissingWebKey)?;
+        // Webkey acquired
 
+        // Construct HTTP client for Steam API
         let client = ClientBuilder::new()
             .user_agent(concat!(
                 "WhatCanWePlay/",
@@ -104,9 +127,15 @@ impl SteamClient {
         }
     }
 
-    pub async fn get_player_summaries(&self, steam_ids: &[u64]) -> std::result::Result<Vec<SteamUser>, SteamError> {
+    /// Fetch the Steam profiles of the given Steam IDs
+    pub async fn get_player_summaries(&self, steam_ids: &[SteamID]) -> std::result::Result<HashMap<SteamID, SteamUser>, SteamError> {
+        if steam_ids.is_empty()
+        {
+            return Ok(Default::default())
+        }
+
         let webkey = &self.1;
-        let steam_ids = steam_ids.iter().format(",").to_string();
+        let steam_ids = steam_ids.iter().format(",").to_string(); // URL format
         let result = self.get(
             "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002"
         )
@@ -142,22 +171,25 @@ impl SteamClient {
         let mut result_json: Value = result.json().await // Top-level value
             .map_err(|err| SteamError::BadSteamResponse(err.to_string()))?;
         
-        let players = result_json["response"]["players"].take(); // Potentially the list of players
+        // Potentially the list of players
+        let players = result_json["response"]["players"].take();
 
-        match serde_json::from_value::<Vec<SteamUser>>(players)
-        {
-            Ok(players) => Ok(players),
-            Err(err) => Err(SteamError::BadSteamResponse(err.to_string()))
-        }
+        // Confirmed as the list of players
+        let players: Vec<SteamUser> = serde_json::from_value(players).map_err(|e| SteamError::BadSteamResponse(e.to_string()))?;
+
+        // Convert to mapping
+        let player_map: HashMap<SteamID,SteamUser> = players.into_iter().map(|user| (user.steam_id, user)).collect();
+
+        Ok(player_map)
     }
 }
 
 #[post("/get_steam_users_info", data = "<steam_ids>")]
-pub async fn get_steam_users_info(steam_ids: Json<Vec<u64>>, client: &State<SteamClient>) -> APIResult<Vec<SteamUser>>
+pub async fn get_steam_users_info(steam_ids: Json<Vec<SteamID>>, client: &State<SteamClient>) -> APIResult<HashMap<SteamID, SteamUser>>
 {
     if steam_ids.is_empty()
     {
-        return Ok(vec![].into());
+        return Ok(HashMap::default().into());
     }
 
     let response = client.get_player_summaries(&steam_ids).await;
