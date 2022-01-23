6c2797efa9430fd0b09c366ca787b1673eb4d998
TGRCDev <tgrc@tgrc.dev>
2022-01-22 17:55:00 -0800

    Restructuring, test for get_steam_users_info

--- src/api/steam/frontend.rs
+++ src/api/steam/frontend.rs
@@ -5,7 +5,7 @@ use super::{
 };
 use rocket::{
     serde::{
-        Serialize, Deserialize,
+        Serialize,
         json::{ Json },
     },
     response::Responder,
@@ -17,6 +17,7 @@ use crate::api::testing::{
     api_test, APITestInfo,
     APITestArgument, APITestArgType::*
 };
+use core::slice::from_ref;
 
 // Errors returned from the WCWP Steam API
 #[derive(Debug, Serialize)]
@@ -79,15 +80,48 @@ impl<'r, 'o: 'r> Responder<'r, 'o> for APIError {
 /// Result type for returning to the HTTP client
 pub type APIResult<T> = std::result::Result<Json<T>, APIError>;
 
+pub mod requests {
+    use serde::Deserialize;
+    use super::super::SteamID;
+
+    #[derive(Deserialize)]
+    #[serde(untagged)]
+    pub enum GetSteamUsersInfoRequest {
+        Single(SteamID),
+        Vec(Vec<SteamID>),
+        Object {
+            steam_ids: Vec<SteamID>,
+        }
+    }
+
+    #[derive(Deserialize)]
+    #[serde(untagged)]
+    pub enum GetFriendsRequest {
+        SteamID(SteamID),
+        Object {
+            steam_id: SteamID,
+            #[serde(default)]
+            get_info: bool,
+        },
+    }
+}
+pub use requests::*;
+
 #[post("/get_steam_users_info", data = "<steam_ids>")]
-pub async fn get_steam_users_info(steam_ids: Json<Vec<SteamID>>, client: &State<SteamClient>) -> APIResult<HashMap<SteamID, SteamUser>>
+pub async fn get_steam_users_info(steam_ids: Json<GetSteamUsersInfoRequest>, client: &State<SteamClient>) -> APIResult<HashMap<SteamID, SteamUser>>
 {
+    let steam_ids = match &*steam_ids {
+        GetSteamUsersInfoRequest::Vec(steam_ids) => steam_ids,
+        GetSteamUsersInfoRequest::Object { steam_ids } => steam_ids,
+        GetSteamUsersInfoRequest::Single(steam_id) => from_ref(steam_id),
+    };
+
     if steam_ids.is_empty()
     {
         return Ok(HashMap::default().into());
     }
 
-    let response = client.get_player_summaries(&steam_ids).await;
+    let response = client.get_player_summaries(steam_ids).await;
     match response {
         Ok(info) => Ok(info.into()),
         Err(err) => {
@@ -96,17 +130,20 @@ pub async fn get_steam_users_info(steam_ids: Json<Vec<SteamID>>, client: &State<
         }
     }
 }
-
-#[derive(Deserialize)]
-#[serde(untagged)]
-pub enum GetFriendsRequest {
-    Type1(SteamID),
-    Type2 {
-        steam_id: SteamID,
-        #[serde(default)]
-        get_info: bool,
-    },
-}
+api_test!(
+    get_steam_users_info_test,
+    "/get_steam_users_info",
+    APITestInfo {
+        func_name: "get_steam_users_info",
+        func_args: vec![
+            APITestArgument {
+                name: "steam_ids",
+                arg_type: CSLOfInt,
+                default: None,
+            }
+        ]
+    }
+);
 
 #[post("/get_friends_list", data = "<request>")]
 pub async fn get_friends_list(request: Json<GetFriendsRequest>, client: &State<SteamClient>) -> APIResult<GetFriendsResponse>
@@ -114,11 +151,11 @@ pub async fn get_friends_list(request: Json<GetFriendsRequest>, client: &State<S
     let steam_id;
     let get_info;
     match *request {
-        GetFriendsRequest::Type1(id) => {
+        GetFriendsRequest::SteamID(id) => {
             steam_id = id;
             get_info = false;
         },
-        GetFriendsRequest::Type2 { steam_id: id, get_info: info } => {
+        GetFriendsRequest::Object { steam_id: id, get_info: info } => {
             steam_id = id;
             get_info = info;
         }
@@ -152,6 +189,7 @@ pub fn routes() -> Vec<Route>
 {
     routes![
         get_steam_users_info,
+        get_steam_users_info_test,
         get_friends_list,
         get_friends_list_test,
     ]
