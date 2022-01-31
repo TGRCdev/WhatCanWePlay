283c6f744e84b9a9c78c67446d2471979a2880fb
TGRCDev <tgrc@tgrc.dev>
2022-01-31 15:55:41 -0800

    The HTML is starting to smell, rewrite

--- static/scripts/fetch_timeout.js
+++
@@ -1,8 +0,0 @@
-function timeout(promise, timeout) {
-    timeout_promise = new Promise((resolve, reject) => {
-        setTimeout(reject, timeout, 'timeout')
-    })
-    return Promise.race([
-        promise, timeout_promise
-    ])
-}
